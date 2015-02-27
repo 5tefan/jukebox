82466a904e4b70289f72da331af86b798f5eae5f
Stefan Codrescu <ssmmcc1@gmail.com>
2015-02-27 20:19:34 +0000

    Structure from yo angular-fullstack generator

---
+++ webapp/client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('jukeboxApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
