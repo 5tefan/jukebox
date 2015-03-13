cb1fe3baa9e90d370f4336d28f863ee07f083f7f
Stefan Codrescu <ssmmcc1@gmail.com>
2015-03-13 22:35:35 +0000

    justin and stefan adding the routes needed to either set up a party (host) or join a party (join) and scaffolding out basic controller methods for each of these

--- webapp/server/api/main/main.controller.js
+++ webapp/server/api/main/main.controller.js
@@ -3,53 +3,30 @@
 var _ = require('lodash');
 var Main = require('./main.model');
 
-// Get list of mains
+//render the home page
 exports.index = function(req, res) {
     return res.render("index");
 };
 
-// Get a single main
-exports.show = function(req, res) {
-  Main.findById(req.params.id, function (err, main) {
-    if(err) { return handleError(res, err); }
-    if(!main) { return res.send(404); }
-    return res.json(main);
-  });
-};
+exports.join_get = function(req, res) {
+	return res.send(200, "coming soon");
+}
 
-// Creates a new main in the DB.
-exports.create = function(req, res) {
-  Main.create(req.body, function(err, main) {
-    if(err) { return handleError(res, err); }
-    return res.json(201, main);
-  });
-};
+exports.join_post = function(req, res) {
+	return res.send(200, "coming soon");
+}
 
-// Updates an existing main in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  Main.findById(req.params.id, function (err, main) {
-    if (err) { return handleError(res, err); }
-    if(!main) { return res.send(404); }
-    var updated = _.merge(main, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
-      return res.json(200, main);
-    });
-  });
-};
+exports.host_get = function(req, res) {
+	return res.send(200, "coming soon");
+}
 
-// Deletes a main from the DB.
-exports.destroy = function(req, res) {
-  Main.findById(req.params.id, function (err, main) {
-    if(err) { return handleError(res, err); }
-    if(!main) { return res.send(404); }
-    main.remove(function(err) {
-      if(err) { return handleError(res, err); }
-      return res.send(204);
-    });
-  });
-};
+exports.host_post = function(req, res) {
+	return res.send(200, "coming soon");
+}
+
+exports.party = function(req, res) {
+	return res.send(200, req.params.name + " soon");
+}
 
 function handleError(res, err) {
   return res.send(500, err);
