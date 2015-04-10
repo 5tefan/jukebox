35ad124c34c8fc28f088bd3c7652c8fb4618b07f
Stefan Codrescu <ssmmcc1@gmail.com>
2015-04-10 22:52:09 +0000

    Justin and Stefan working on getting the routes to actually render views. Set up both views host and join with functional routes for the GET and POST actions server side. Party name is now added to the database with conditional rejection for party names that have already been used.

--- webapp/server/api/main/main.controller.js
+++ webapp/server/api/main/main.controller.js
@@ -3,26 +3,63 @@
 var _ = require('lodash');
 var Main = require('./main.model');
 
+var mongoose = require('mongoose');
+var Party = require('../party/party.model');
+
 //render the home page
 exports.index = function(req, res) {
     return res.render("index");
 };
 
-exports.join_get = function(req, res) {
-	return res.send(200, "coming soon");
-}
-
-exports.join_post = function(req, res) {
-	return res.send(200, "coming soon");
-}
-
 exports.host_get = function(req, res) {
-	return res.send(200, "coming soon");
+	return res.render("host");
 }
 
 exports.host_post = function(req, res) {
-	return res.send(200, "coming soon");
+	console.log(req.body.name);
+	var party_name = req.body.name.toLowerCase();
+	Party.findOne( { name: party_name }, function(err, party) {
+		if (party) { //party name already taken
+			return res.render("host", {message: "Party identifier already in use"});
+		} else { //make the party
+			var insert = new Party({
+				name: party_name
+			});
+			insert.save( function(err, party) {
+				res.cookie('isdj', true);
+				res.cookie('id', party._id);
+				return res.redirect("/p/" + party.name);
+			});
+		}
+	} );
 }
+
+exports.join_get = function(req, res) {
+	return res.render("join");
+}
+
+exports.join_post = function(req, res) {
+	console.log(req.body.name);
+	var party_name = req.body.name.toLowerCase();
+	Party.findOne( { name: party_name }, function(err, party) {
+		if (party) { // party was found, user can join
+			var id = new mongoose.Types.ObjectId;
+
+			//update the database
+			party.users.push({id: id});
+			party.markModified('party.users');
+			party.save( function(err, party) {
+				res.cookie('isdj', false);
+				res.cookie('id', id);
+				return res.redirect("/p/" + party.name);
+			});
+
+		} else { //party not found
+			return res.render("join", {message: "That party doesn't exist!"});
+		}
+	} );
+}
+
 
 exports.party = function(req, res) {
 	return res.sendfile(req.app.get('appPath')+'/party.html');
